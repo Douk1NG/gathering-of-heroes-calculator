1d02fcf8166f1c7f345f4f52c75594639afaf74f
aLxVa <dialexanderx@gmail.com>
2026-02-12 13:45:56 +0100

    title adjustment

--- src/components/header/TitleSection.tsx
+++ src/components/header/TitleSection.tsx
@@ -4,9 +4,9 @@ export function TitleSection() {
   return (
     <div className="text-center lg:text-left">
       <h1 className="text-4xl md:text-6xl font-black tracking-tighter bg-clip-text text-transparent bg-linear-to-br from-white via-white to-white/40 mb-2">
-        {t(translations.header.title).split(' ')[0]}{' '}
-        {t(translations.header.title).split(' ').slice(1).join(' ')} OF{' '}
-        <span className="text-yellow-500">{t(translations.header.subtitle).split(' ')[0]}</span>
+        {t(translations.header.title)}
+        <br />
+        <span className="text-yellow-500">{t(translations.header.subtitle)}</span>
       </h1>
       <p className="text-neutral-400 text-lg max-w-xl">{t(translations.header.disclaimer)}</p>
     </div>
